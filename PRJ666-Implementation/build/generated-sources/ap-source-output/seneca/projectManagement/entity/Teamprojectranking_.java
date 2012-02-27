def4607b42e1570a89f29c1fddf99b2759bc0e28
Matthew Schranz <schranz.m@gmail.com>
2012-02-27 10:06:46 -0500

    More silly auto generated class stuff before deploying new version of project

--- PRJ666-Implementation/build/generated-sources/ap-source-output/seneca/projectManagement/entity/Teamprojectranking_.java
+++ PRJ666-Implementation/build/generated-sources/ap-source-output/seneca/projectManagement/entity/Teamprojectranking_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-02-26T12:16:37")
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-02-27T10:06:33")
 @StaticMetamodel(Teamprojectranking.class)
 public class Teamprojectranking_ { 
 
